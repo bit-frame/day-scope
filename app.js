31f1996923ff1c575626ba5eccb1561909f5a2e2
bit-frame <linuskang09@gmail.com>
2024-12-11 17:14:40 +1000

    Applied fix for CORS

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express');
 require('dotenv').config();
-const path = require('path')
-const api = require('./routes/api')
+const path = require('path');
+const yaml = require('yaml');
+const fs = require('fs');
+const api = require('./routes/api');
+const cors = require('cors');
+
+const configFile = fs.readFileSync('config.yaml', 'utf8');
+const config = yaml.parse(configFile);
+
 const API_TOKEN = process.env.API_KEY;
 
+const { website, administrator, api: apiConfig, version, build } = config;
+const httpUrl = `http://${website.url}:${website.port}`;
+
+const corsOptions = {
+    origin: [httpUrl],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'api_token']
+};
+
 const app = express();
-const port = 3000;
+const port = website.port;
+
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(api)
+app.use(api);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
@@ -27,5 +45,5 @@ app.use((req, res) => {
 });
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${port}`);
+    console.log(`[INFO] DayScope Server ${version} Build ${build}\n> Access at ${website.url}:${port}`);
 });
