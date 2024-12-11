c4f047b87ca0813538f96b41a52adcf158e68009
bit-frame <linuskang09@gmail.com>
2024-12-11 13:16:26 +1000

    Added main API endpoints

--- 12.24/build-1/DayScope/app.js
+++ 12.24/build-1/DayScope/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 const path = require('path')
 const api = require('./routes/api')
+const API_TOKEN = process.env.API_KEY;
 
 const app = express();
 const port = 3000;
@@ -10,6 +12,10 @@ app.use(api)
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+app.get('/api', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'apicall.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
--- 12.24/build-1/DayScope/routes/api.js
+++ 12.24/build-1/DayScope/routes/api.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.all('/api/status', (req, res, next) => {
+router.all(['/v1/api/status', '/v1/api/data'], (req, res, next) => {
+
     if (req.method !== 'GET') {
-        return res.status(403).json({ message: 'Method Not Allowed: Only GET requests are allowed' });
+        return res.status(403).json({ message: 'Method Not Allowed' });
     }
+
+    const apiKey = req.headers['api_token'];
+    if (!apiKey || apiKey !== process.env.API_KEY) {
+        return res.status(403).json({ message: '403 Forbidden' });
+    }
+
     next();
 });
 
-router.get('/api/status', (req, res) => {
+router.get('/v1/api/status', (req, res) => {
     res.json({ status: 'API is up and running' });
+});
+
+router.get('/v1/api/data', (req, res) => {
+    res.json({ status: 'data :o' });
 });
 
 module.exports = router;
