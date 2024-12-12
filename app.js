6fef0df3b1afc12d32fd51a604f5536f98ce601e
bit-frame <linuskang09@gmail.com>
2024-12-12 10:27:23 +1000

    Updated to 2024.12 Build 2 beta

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
 require('dotenv').config();
+
+const express = require('express');
 const path = require('path');
 const yaml = require('yaml');
 const fs = require('fs');
 const api = require('./routes/api');
 const cors = require('cors');
+const sendWebhook = require('./webhook')
 
 const configFile = fs.readFileSync('config.yaml', 'utf8');
 const config = yaml.parse(configFile);
-
-const sendWebhook = require('./webhook')
-
 const API_TOKEN = process.env.API_KEY;
-
 const { website, administrator, api: apiConfig, version, build} = config;
 const sendStartup = config.webhooks.logs.sendStartup
 const httpUrl = `http://${website.url}:${website.port}`;
+const app = express();
+const port = website.port;
 
 const corsOptions = {
     origin: [httpUrl, `http://localhost:${website.port}`],
@@ -23,29 +23,16 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'api_token']
 };
 
-const app = express();
-const port = website.port;
-
 app.use(cors(corsOptions));
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(api);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
-});
-
-app.get('/api', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'apicall.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-
-app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'home.html'))});
+app.get('/home', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'about.html'))});
+app.get('/api', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'apicall.html'))});
+app.get('/login', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'login.html'))});
+app.get('/dashboard', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'dashboard', 'dashboard.html'))});
+app.use((req, res) => { res.status(404).sendFile(path.join(__dirname, 'public', '404.html'))});
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`[INFO] DayScope Server ${version} Build ${build}\n> Access at ${website.url}:${port}`);
